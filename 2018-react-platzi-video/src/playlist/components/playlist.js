34be88607cc9e2c00e0b7df4016c5d799564366d
Kelvin Lemus <kelvin932810@gmail.com>
2018-06-03 23:18:56 -0600

    :v

--- 2018-react-platzi-video/src/playlist/components/playlist.js
+++ 2018-react-platzi-video/src/playlist/components/playlist.js
@@ -3,13 +3,10 @@ import Media from './media.js'
 import './playlist.css'
 
 function Playlist (props) {
-  const playlist = props.data.categories[0].playlist
-
-  console.log('data', props.data)
   return (
     <div className='Playlist'>
       {
-        playlist.map((item) => {
+        props.playlist.map((item) => {
           return <Media key={item.id} {...item} />
         })
       }
